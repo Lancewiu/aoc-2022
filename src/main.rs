d195a289c223c6b53fd11e8d69b9b4a505c3ee7c
Lawrence Niu <ranchwiu@gmail.com>
2022-12-03 16:21:12 -0500

    complete day 3 part 2

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,28 @@ const FILENAME: &str = "data/input.txt";
 
 fn process_lines(reader: impl BufRead) -> anyhow::Result<u64> {
     let mut priorities: u64 = 0;
-    for line_result in reader.lines() {
-        let rucksack: Vec<char> = line_result?.chars().collect();
-        let second_compartment_index = rucksack.len() / 2;
-        let mut first_rucksack_types = (&rucksack[..second_compartment_index]).to_vec();
-        first_rucksack_types.sort();
-        first_rucksack_types.dedup();
-        let common_item = rucksack[second_compartment_index..]
-            .iter()
-            .find(|&item| first_rucksack_types.contains(item))
-            .copied()
+    let mut first_sack: Vec<char> = Vec::new();
+    let mut second_sack: Vec<char> = Vec::new();
+    for (i_line, line_result) in reader.lines().enumerate() {
+        let mut rucksack: Vec<char> = line_result?.chars().collect();
+        if 2 != i_line % 3 {
+            if 0 == i_line % 3 {
+                first_sack = rucksack;
+            } else {
+                second_sack = rucksack;
+            }
+            continue;
+        }
+        first_sack.sort();
+        first_sack.dedup();
+        second_sack.sort();
+        second_sack.dedup();
+        rucksack.sort();
+        rucksack.dedup();
+
+        let common_item = rucksack
+            .into_iter()
+            .find(|item| first_sack.contains(item) && second_sack.contains(item))
             .ok_or_else(|| anyhow::Error::msg("no common item found"))?;
         let item_priority = (common_item as u32)
             - if common_item.is_ascii_uppercase() {
