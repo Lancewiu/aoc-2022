822a3ada847fea8c19d02cadea87a2519028a4ca
Lawrence Niu <ranchwiu@gmail.com>
2022-12-03 14:24:47 -0500

    complete day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::str;
+
+const FILENAME: &str = "data/input.txt";
+
+fn process_lines(reader: impl BufRead) -> anyhow::Result<u64> {
+    let mut elf_sum: Vec<u64> = Vec::new();
+    let mut elf: Vec<u64> = Vec::new();
+    for line_result in reader.lines() {
+        let line = line_result?;
+        if line.is_empty() {
+            elf_sum.push(elf.drain(..).sum());
+            continue;
+        }
+
+        elf.push(line.parse()?);
+    }
+    Ok(elf_sum
+        .into_iter()
+        .max()
+        .ok_or(anyhow::Error::msg("no elves?"))?)
+}
+
 fn main() {
-    println!("Hello, world!");
+    match File::open(FILENAME) {
+        Ok(file) => match process_lines(BufReader::new(file)) {
+            Err(err) => {
+                eprintln!("Could not process file {}:\n  {}", FILENAME, err);
+            }
+            Ok(calories) => {
+                println!("max calories: {}", calories);
+            }
+        },
+        Err(err) => {
+            eprintln!("Error opening file {}:\n  {}", FILENAME, err);
+        }
+    }
 }
