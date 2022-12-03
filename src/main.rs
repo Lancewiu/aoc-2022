46a0b33d1cc1777902bd09f322e94351a0badd71
Lawrence Niu <ranchwiu@gmail.com>
2022-12-03 16:00:06 -0500

    complete day 3 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-mod rps;
-
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::str;
@@ -7,24 +5,27 @@ use std::str;
 const FILENAME: &str = "data/input.txt";
 
 fn process_lines(reader: impl BufRead) -> anyhow::Result<u64> {
-    let mut score: u64 = 0;
-    let get_first_char = |token_opt: Option<&str>| -> anyhow::Result<char> {
-        token_opt
-            .and_then(|s| s.chars().next())
-            .ok_or_else(|| anyhow::Error::msg("invalid token string"))
-    };
+    let mut priorities: u64 = 0;
     for line_result in reader.lines() {
-        let line = line_result?;
-        let mut tokens = line.split_ascii_whitespace();
-        let play = rps::Shape::try_from_play(get_first_char(tokens.next())?)
-            .ok_or_else(|| anyhow::Error::msg("invalid play character"))?;
-        let outcome = rps::Outcome::try_from_strategy(get_first_char(tokens.next())?)
-            .ok_or_else(|| anyhow::Error::msg("invalid outcome character"))?;
-
-        let strategy = rps::Shape::from_strategy(&play, &outcome);
-        score += strategy.score() + outcome.score();
+        let rucksack: Vec<char> = line_result?.chars().collect();
+        let second_compartment_index = rucksack.len() / 2;
+        let mut first_rucksack_types = (&rucksack[..second_compartment_index]).to_vec();
+        first_rucksack_types.sort();
+        first_rucksack_types.dedup();
+        let common_item = rucksack[second_compartment_index..]
+            .iter()
+            .find(|&item| first_rucksack_types.contains(item))
+            .copied()
+            .ok_or_else(|| anyhow::Error::msg("no common item found"))?;
+        let item_priority = (common_item as u32)
+            - if common_item.is_ascii_uppercase() {
+                38
+            } else {
+                96
+            };
+        priorities += item_priority as u64;
     }
-    Ok(score)
+    Ok(priorities)
 }
 
 fn main() {
@@ -33,8 +34,8 @@ fn main() {
             Err(err) => {
                 eprintln!("Could not process file {}:\n  {}", FILENAME, err);
             }
-            Ok(score) => {
-                println!("score: {}", score);
+            Ok(priorities) => {
+                println!("priorities: {}", priorities);
             }
         },
         Err(err) => {
