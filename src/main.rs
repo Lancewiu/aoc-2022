f4303cd3659fcecdce2bc559425536cd7d038c81
Lawrence Niu <ranchwiu@gmail.com>
2022-12-07 00:10:48 -0500

    complete day 6 part 2

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,30 @@ use std::str;
 
 const FILENAME: &str = "data/input.txt";
 
+fn is_buffer_unique(buffer: &[u8]) -> bool {
+    let mut unique_buffer = buffer.to_vec();
+    unique_buffer.sort();
+    unique_buffer.dedup();
+    unique_buffer.len() == buffer.len()
+}
+
 fn process_lines(reader: impl BufRead) -> anyhow::Result<usize> {
-    let mut buffer: Vec<u8> = Vec::with_capacity(5);
+    let mut buffer = Vec::with_capacity(15);
     for (i, byte_res) in reader.bytes().enumerate() {
-        buffer.push(byte_res?);
-        if buffer.len() < 4 {
+        let byte = byte_res?;
+        buffer.push(byte);
+        if buffer.len() < 14 {
             continue;
         }
 
-        if buffer.len() > 4 {
+        if buffer.len() > 14 {
             buffer.remove(0);
         }
-
-        let mut unique_buffer = buffer.clone();
-        unique_buffer.sort();
-        unique_buffer.dedup();
-        if 4 == unique_buffer.len() {
+        if is_buffer_unique(&buffer[..]) {
             return Ok(i + 1);
         }
     }
-    Err(anyhow::Error::msg("failed to find a valid index!"))
+    Err(anyhow::Error::msg("could not find valid message index!"))
 }
 
 fn main() {
@@ -32,8 +36,8 @@ fn main() {
             Err(err) => {
                 eprintln!("Could not process file {}:\n  {}", FILENAME, err);
             }
-            Ok(i_start_of_packet) => {
-                println!("start-of-packet index: {}", i_start_of_packet);
+            Ok(i_start_of_message) => {
+                println!("start-of-message index: {}", i_start_of_message);
             }
         },
         Err(err) => {
