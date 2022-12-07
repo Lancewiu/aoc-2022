c6e049e913695eac7be272360e81a5ee79a9f6a5
Lawrence Niu <ranchwiu@gmail.com>
2022-12-07 01:36:59 -0500

    begin day 7 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,86 @@
+mod elffs;
+
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::str;
 
+type DirectoryMap = HashMap<String, elffs::Directory>;
+
 const FILENAME: &str = "data/input.txt";
 
-fn is_buffer_unique(buffer: &[u8]) -> bool {
-    let mut unique_buffer = buffer.to_vec();
-    unique_buffer.sort();
-    unique_buffer.dedup();
-    unique_buffer.len() == buffer.len()
+fn get_directory_size(directory_map: &DirectoryMap, directory_name: &str) -> usize {
+    directory_map
+        .get(&directory_name.to_string())
+        .map(|dir| {
+            dir.get_file_sizes()
+                + dir
+                    .get_subdirectory_names()
+                    .iter()
+                    .map(|subdir| get_directory_size(directory_map, subdir.as_str()))
+                    .sum::<usize>()
+        })
+        .unwrap_or(0)
 }
 
 fn process_lines(reader: impl BufRead) -> anyhow::Result<usize> {
-    let mut buffer = Vec::with_capacity(15);
-    for (i, byte_res) in reader.bytes().enumerate() {
-        let byte = byte_res?;
-        buffer.push(byte);
-        if buffer.len() < 14 {
-            continue;
-        }
+    let mut name_to_directory: DirectoryMap = HashMap::new();
+    let mut name_to_parent_name: HashMap<String, String> = HashMap::new();
+    let mut current_dir_name = "/".to_string();
+    name_to_directory
+        .entry(current_dir_name.clone())
+        .or_default();
+    for line_res in reader.lines() {
+        let line = line_res?;
+        let mut tokens = line.split_ascii_whitespace();
+        let context = tokens.next().expect("unexpected empty line");
 
-        if buffer.len() > 14 {
-            buffer.remove(0);
-        }
-        if is_buffer_unique(&buffer[..]) {
-            return Ok(i + 1);
+        match context {
+            "$" => {
+                let command = tokens.next().expect("invalid command");
+                match command {
+                    "cd" => {
+                        let dir = tokens
+                            .next()
+                            .expect("`cd` command missing argument")
+                            .to_string();
+                        current_dir_name = if ".." == dir {
+                            name_to_parent_name[&current_dir_name].clone()
+                        } else {
+                            dir.clone()
+                        };
+                    }
+                    "ls" => {}
+                    tok => {
+                        panic!("unexpected token `{}` encountered", tok);
+                    }
+                }
+            }
+            "dir" => {
+                let dir_name = tokens.next().expect("missing dir name").to_string();
+                name_to_parent_name.insert(dir_name.clone(), current_dir_name.clone());
+                name_to_directory.entry(dir_name.clone()).or_default();
+                name_to_directory
+                    .get_mut(&current_dir_name)
+                    .expect("current directory not defined! could not add directory")
+                    .add_directory(dir_name.as_str());
+            }
+            size_str => {
+                let file_name = tokens.next().expect("missing file name");
+                let file_size: usize = size_str.parse().expect("invalid file size value");
+                name_to_directory
+                    .get_mut(&current_dir_name)
+                    .expect("current directory not defined! could not add file")
+                    .add_file(elffs::File::new(file_name.to_string(), file_size));
+            }
         }
     }
-    Err(anyhow::Error::msg("could not find valid message index!"))
+
+    Ok(name_to_directory
+        .keys()
+        .map(|name| get_directory_size(&name_to_directory, name.as_str()))
+        .filter(|dir_size| *dir_size <= 100_000)
+        .sum())
 }
 
 fn main() {
@@ -36,8 +89,8 @@ fn main() {
             Err(err) => {
                 eprintln!("Could not process file {}:\n  {}", FILENAME, err);
             }
-            Ok(i_start_of_message) => {
-                println!("start-of-message index: {}", i_start_of_message);
+            Ok(sum) => {
+                println!("sum: {}", sum);
             }
         },
         Err(err) => {
