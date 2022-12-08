02f4b7ead96ad914f8f160b9ba84fa8011088939
Lawrence Niu <ranchwiu@gmail.com>
2022-12-07 21:41:57 -0500

    complete day 7 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,15 @@
-mod elffs;
-
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::str;
 
-type DirectoryMap = HashMap<String, elffs::Directory>;
-
 const FILENAME: &str = "data/input.txt";
-
-fn get_directory_size(directory_map: &DirectoryMap, directory_name: &str) -> usize {
-    directory_map
-        .get(&directory_name.to_string())
-        .map(|dir| {
-            dir.get_file_sizes()
-                + dir
-                    .get_subdirectory_names()
-                    .iter()
-                    .map(|subdir| get_directory_size(directory_map, subdir.as_str()))
-                    .sum::<usize>()
-        })
-        .unwrap_or(0)
-}
+const DELIMITER: &str = "/";
 
 fn process_lines(reader: impl BufRead) -> anyhow::Result<usize> {
-    let mut name_to_directory: DirectoryMap = HashMap::new();
-    let mut name_to_parent_name: HashMap<String, String> = HashMap::new();
-    let mut current_dir_name = "/".to_string();
-    name_to_directory
-        .entry(current_dir_name.clone())
-        .or_default();
+    let mut path_to_size: HashMap<String, usize> = HashMap::new();
+    let mut current_path = vec!["".to_string()];
+
     for line_res in reader.lines() {
         let line = line_res?;
         let mut tokens = line.split_ascii_whitespace();
@@ -40,15 +20,20 @@ fn process_lines(reader: impl BufRead) -> anyhow::Result<usize> {
                 let command = tokens.next().expect("invalid command");
                 match command {
                     "cd" => {
-                        let dir = tokens
-                            .next()
-                            .expect("`cd` command missing argument")
-                            .to_string();
-                        current_dir_name = if ".." == dir {
-                            name_to_parent_name[&current_dir_name].clone()
+                        let dir = tokens.next().expect("`cd` command missing argument");
+                        if ".." == dir {
+                            if 1 < current_path.len() {
+                                current_path.pop();
+                            }
                         } else {
-                            dir.clone()
-                        };
+                            let mut new_path: Vec<String> =
+                                dir.split(DELIMITER).map(|s| s.to_string()).collect();
+                            if new_path[0].is_empty() {
+                                current_path = new_path;
+                            } else {
+                                current_path.append(&mut new_path);
+                            }
+                        }
                     }
                     "ls" => {}
                     tok => {
@@ -56,30 +41,25 @@ fn process_lines(reader: impl BufRead) -> anyhow::Result<usize> {
                     }
                 }
             }
-            "dir" => {
-                let dir_name = tokens.next().expect("missing dir name").to_string();
-                name_to_parent_name.insert(dir_name.clone(), current_dir_name.clone());
-                name_to_directory.entry(dir_name.clone()).or_default();
-                name_to_directory
-                    .get_mut(&current_dir_name)
-                    .expect("current directory not defined! could not add directory")
-                    .add_directory(dir_name.as_str());
-            }
+            "dir" => {}
             size_str => {
-                let file_name = tokens.next().expect("missing file name");
                 let file_size: usize = size_str.parse().expect("invalid file size value");
-                name_to_directory
-                    .get_mut(&current_dir_name)
-                    .expect("current directory not defined! could not add file")
-                    .add_file(elffs::File::new(file_name.to_string(), file_size));
+                let num_paths = current_path.len();
+                for right_offset in (1..=num_paths).rev() {
+                    path_to_size
+                        .entry(current_path[..right_offset].join(DELIMITER))
+                        .and_modify(|size| {
+                            *size += file_size;
+                        })
+                        .or_insert(file_size);
+                }
             }
         }
     }
 
-    Ok(name_to_directory
-        .keys()
-        .map(|name| get_directory_size(&name_to_directory, name.as_str()))
-        .filter(|dir_size| *dir_size <= 100_000)
+    Ok(path_to_size
+        .values()
+        .filter(|dir_size| **dir_size <= 100_000)
         .sum())
 }
 
