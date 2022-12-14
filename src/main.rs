0b852aa09e9efb0172e74f0db9f52d7656a184fa
Lawrence Niu <ranchwiu@gmail.com>
2022-12-13 19:42:38 -0500

    complete day 10 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,35 @@
-mod grid;
-mod rope;
-
-use grid::{Coordinate, Direction};
-use rope::Rope;
-
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 const FILENAME: &str = "data/input.txt";
 
-fn process_lines(reader: impl BufRead) -> anyhow::Result<usize> {
-    let mut rope = Rope::new(Coordinate(0, 0));
-    let mut tail_positions: Vec<Coordinate> = Vec::new();
+fn process_lines(reader: impl BufRead) -> anyhow::Result<i64> {
+    let mut register = 1_i64;
+    let mut cycle = 0_usize;
+    let cycle_query = [19_usize, 59, 99, 139, 179, 219];
+    let mut signal_sum = 0;
     for line_result in reader.lines() {
         let line = line_result?;
-        let mut tokens = line.split_ascii_whitespace();
-        let direction = tokens
-            .next()
-            .and_then(|dir_str| dir_str.chars().next())
-            .and_then(Direction::try_from_char)
-            .ok_or_else(|| anyhow::Error::msg("malformed direction input."))?;
-        let distance: usize = tokens
-            .next()
-            .and_then(|dist_str| dist_str.parse().ok())
-            .ok_or_else(|| anyhow::Error::msg("malformed distance input."))?;
-        (0..distance).for_each(|_| {
-            rope.shift(direction);
-            tail_positions.push(rope.get_tail());
-        });
+        let cost = if "noop" == line { 1 } else { 2 };
+
+        for i_cycle in cycle..(cycle + cost) {
+            if cycle_query.binary_search(&i_cycle).is_ok() {
+                signal_sum += (1 + i_cycle as i64) * register;
+            }
+        }
+
+        cycle += cost;
+        if "noop" != line {
+            let mut tokens = line.split_ascii_whitespace().skip(1);
+            let addend: i64 = tokens
+                .next()
+                .and_then(|add_str| add_str.parse().ok())
+                .ok_or_else(|| anyhow::Error::msg("failed to read addend."))?;
+            register += addend;
+        }
     }
-    tail_positions.sort();
-    tail_positions.dedup();
-    Ok(tail_positions.len())
+
+    Ok(signal_sum)
 }
 
 fn main() {
