c1051ff76cdebac9b8135a40a9be776cde6aa98b
Lawrence Niu <ranchwiu@gmail.com>
2022-12-28 18:22:51 -0500

    start day 11 refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,103 @@
+mod calculator;
+
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::str::{self, FromStr};
 
-const FILENAME: &str = "data/input.txt";
+const FILENAME: &str = "data/test_input.txt";
 
-fn process_lines(reader: impl BufRead) -> anyhow::Result<()> {
-    let mut sprite_column = 1_i64;
-    let mut cycle = 0_usize;
-    let mut is_pixel_bright = [false; 240];
+struct Monkey {
+    id: usize,
+    items: Vec<i64>,
+    operation: calculator::Phrase,
+    test: Box<dyn FnOnce(i64) -> bool>,
+    test_pass_destination: usize,
+    test_fail_destination: usize,
+}
+
+fn process_test(test: &str) -> anyhow::Result<Box<dyn FnOnce(i64) -> bool>> {
+    let divisor: i64 = test
+        .split_whitespace()
+        .nth(3)
+        .and_then(|div_str| div_str.parse().ok())
+        .ok_or_else(|| anyhow::Error::msg("malformed test: no divisor found"))?;
+    Ok(if 0 == divisor {
+        Box::new(|_| false)
+    } else {
+        Box::new(move |var: i64| 0 == var % divisor)
+    })
+}
+
+fn process_starting_items(items: &str) -> anyhow::Result<Vec<i64>> {
+    items
+        .trim()
+        .split(',')
+        .map(|item| {
+            item.trim()
+                .parse()
+                .map_err(|_| anyhow::Error::msg("failed to parse starting items"))
+        })
+        .collect()
+}
+
+impl Monkey {
+    fn try_from_lines(lines: &[String]) -> anyhow::Result<Self> {
+        if lines.len() < 6 {
+            return Err(anyhow::Error::msg(format!(
+                "invalid # of lines {} != 6",
+                lines.len()
+            )));
+        }
+        let (id_str, _) = lines[0]
+            .split_once(':')
+            .ok_or_else(|| anyhow::Error::msg("no id found"))?;
+        let (_, item_str) = lines[1]
+            .split_once(':')
+            .ok_or_else(|| anyhow::Error::msg("missing starting items list"))?;
+        let (_, operation_str) = lines[2]
+            .split_once(':')
+            .ok_or_else(|| anyhow::Error::msg("missing operation function"))?;
+        Ok(Monkey {
+            id: id_str
+                .split_whitespace()
+                .nth(1)
+                .and_then(|id_token| id_token.parse().ok())
+                .ok_or_else(|| anyhow::Error::msg("invalid id found"))?,
+            items: process_starting_items(item_str)?,
+            operation: calculator::Phrase::from_str(operation_str)?,
+            test: process_test(lines[3].as_str())?,
+            test_pass_destination: lines[4]
+                .split_whitespace()
+                .nth(5)
+                .and_then(|id_str| id_str.parse().ok())
+                .ok_or_else(|| anyhow::Error::msg("invalid success destination"))?,
+            test_fail_destination: lines[5]
+                .split_whitespace()
+                .nth(5)
+                .and_then(|id_str| id_str.parse().ok())
+                .ok_or_else(|| anyhow::Error::msg("invalid failure destination"))?,
+        })
+    }
+}
+
+fn process_lines(reader: impl BufRead) -> anyhow::Result<Vec<Monkey>> {
+    let mut monkey_lines: Vec<Vec<String>> = Vec::new();
+    let mut monkey_raw: Vec<String> = Vec::new();
     for line_result in reader.lines() {
         let line = line_result?;
-        let cost = if "noop" == line { 1 } else { 2 };
-
-        for i_cycle in cycle..(cycle + cost) {
-            is_pixel_bright[i_cycle] = (-1..=1)
-                .map(|i_sprite| i_sprite + sprite_column)
-                .any(|i_sprite| (i_cycle as i64 % 40) == i_sprite);
-        }
-
-        cycle += cost;
-        if "noop" != line {
-            let mut tokens = line.split_ascii_whitespace().skip(1);
-            let addend: i64 = tokens
-                .next()
-                .and_then(|add_str| add_str.parse().ok())
-                .ok_or_else(|| anyhow::Error::msg("failed to read addend."))?;
-            sprite_column += addend;
+        if line.is_empty() {
+            monkey_lines.push(monkey_raw.clone());
+            monkey_raw.clear();
+        } else {
+            monkey_raw.push(line);
         }
     }
+    monkey_lines.push(monkey_raw);
 
-    let display_buffer: Vec<char> = is_pixel_bright
+    monkey_lines
         .into_iter()
-        .map(|is_bright| if is_bright { '#' } else { '.' })
-        .collect();
-    (0usize..6)
-        .map(|i_row| {
-            let i_start = i_row * 40;
-            let i_end = i_start + 40;
-            i_start..i_end
-        })
-        .for_each(|line_indices| {
-            println!(
-                "{}",
-                display_buffer[line_indices].iter().collect::<String>()
-            );
-        });
-
-    Ok(())
+        .map(|lines| Monkey::try_from_lines(&lines[..]))
+        .collect()
 }
 
 fn main() {
