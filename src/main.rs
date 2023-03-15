8a5fc0aea57552099300433ceada3f5470ad337a
Lawrence Niu <ranchwiu@gmail.com>
2023-03-14 22:04:38 -0400

    Complete main loop and Part 1

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,34 @@ mod parser;
 use std::fs::File;
 use std::io::BufReader;
 
-const FILENAME: &str = "data/test_input.txt";
+const FILENAME: &str = "data/input.txt";
 
-fn simulate_anxiety(monkeys: &[monkey::Monkey]) -> u64 {
+fn simulate_anxiety(monkeys: &mut [monkey::Monkey]) -> u64 {
+    let mut inspection_counter: Vec<u64> = vec![0; monkeys.len()];
 
+    for i_round in 0..20 {
+        for i_monkey in 0..monkeys.len() {
+            'turn: loop {
+                let new_worry = if let Some(worry) = monkeys[i_monkey].throw_current_item() {
+                    monkeys[i_monkey].inspect_item(worry) / 3
+                } else {
+                    break 'turn;
+                };
+                inspection_counter[i_monkey] += 1;
+                let next_monkey = monkeys[i_monkey].test_anxiety(new_worry);
+                monkeys[next_monkey].catch_item(new_worry);
+            }
+        }
+        println!("After round {}:", i_round + 1);
+        monkeys
+            .iter()
+            .enumerate()
+            .for_each(|(i, m)| println!("Monkey: {}: {:?}", i, m));
+    }
+
+    inspection_counter.sort_unstable();
+    inspection_counter.reverse();
+    inspection_counter[0] * inspection_counter[1]
 }
 
 fn main() {
@@ -16,8 +40,8 @@ fn main() {
             Err(err) => {
                 eprintln!("Could not process file {}:\n  {}", FILENAME, err);
             }
-            Ok(monkeys) => {
-                println!("Multiple of Anxieties {}", simulate_anxiety(&monkeys[..]))
+            Ok(mut monkeys) => {
+                println!("Anxiety Score {}", simulate_anxiety(&mut monkeys[..]))
             }
         },
         Err(err) => {
